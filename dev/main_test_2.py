e69cdcc92ca77bbade8802a7ec96ef1b18193028
Chain <chensunmac@gmail.com>
2017-11-01 16:36:41 -0400

    finished uploading

--- dev/main_test_2.py
+++ dev/main_test_2.py
@@ -7,20 +7,30 @@ Created on Tue Oct 31 14:59:45 2017
 
 import sys
 import Algorithms.AlgorithmSet as AlgSet
+
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+
 from PyQt5 import QtCore, QtGui
 from PyQt5.QtWidgets import (QApplication, QMainWindow, QToolTip, 
                              QPushButton,QDesktopWidget, QAction,
                              QProgressBar, QCheckBox, QMessageBox,
-                             QTextEdit, QFileDialog)
+                             QTextEdit, QFileDialog, QSizePolicy)
+
+import random
+
 
 class Window(QMainWindow):
 
     def __init__(self):
         super(Window, self).__init__()
-        self.setGeometry(50, 50, 500, 300)
+        self.setGeometry(50, 50, 500, 700)
         self.setWindowTitle('BlueNose Analyzer')
         self.setWindowIcon(QtGui.QIcon('Kakyoin.png'))
 
+        self.data = DataHolder()
+        
         extractAction = QAction("&GET TO THE CHOPPAH!!!", self)
         extractAction.setShortcut("Ctrl+Q")
         extractAction.setStatusTip('Leave The App')
@@ -83,6 +93,10 @@ class Window(QMainWindow):
         self.btn = QPushButton("Download",self)
         self.btn.move(200,120)
         self.btn.clicked.connect(self.download)
+        ### Plot Canvas
+        
+        self.canvas = PlotCanvas(self, width=5, height=4)
+        self.canvas.move(200,200)
 
         self.show()
 
@@ -98,9 +112,9 @@ class Window(QMainWindow):
 
     def enlarge_window(self, state):
         if state == QtCore.Qt.Checked:
-            self.setGeometry(50,50, 1000, 600)
+            self.setGeometry(50,50, 1000, 800)
         else:
-            self.setGeometry(50, 50, 500, 300)
+            self.setGeometry(50, 50, 500, 700)
         self.center()
         
     def center(self):
@@ -131,9 +145,12 @@ class Window(QMainWindow):
         with open(file_name[0], "rb") as bin_file:
             signal_matrices = AlgSet.processBinFile(bin_file)
         
-        self.editor()
-        text = str(signal_matrices[0,0,:])
-        self.textEdit.setText(text)
+        #self.editor()
+        #text = str(signal_matrices[0,0,:])
+        #self.textEdit.setText(text)
+        distanceMap = AlgSet.calliperAlg(signal_matrices)
+        #self.canvas.plot_data(signal_matrices[0,0,:])
+        self.canvas.plot_data(distanceMap)
         
     def close_application(self):
         choice = QMessageBox.question(self, 'Extract!',
@@ -144,10 +161,54 @@ class Window(QMainWindow):
             sys.exit()
         else:
             pass
-        
-        
+  
 
+class PlotCanvas(FigureCanvas):
+ 
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+ 
+        FigureCanvas.__init__(self, fig)
+        self.setParent(parent)
+ 
+        FigureCanvas.setSizePolicy(self,
+                QSizePolicy.Expanding,
+                QSizePolicy.Expanding)
+        FigureCanvas.updateGeometry(self)
+        #self.plot()
+ 
+ 
+    def plot(self):
+        data = [random.random() for i in range(25)]
+        ax = self.figure.add_subplot(111)
+        ax.plot(data, 'r-')
+        ax.set_title('PyQt Matplotlib Example')
+        self.draw()      
+
+    def plot_data(self,data):
+        #ax = self.figure.add_subplot(111)
+        fig = plt.figure()
+        plt.imshow(data, aspect='auto');
+        plt.colorbar()
+        plt.show()
+#        ax.plot(data, 'r-')
+#        ax.set_title('One single 2000 point signal')
+        fig.show()
+        self.draw()         
+
+
+class DataHolder(object):
+    """
+    Hold data for plot and other interfaces.
     
+    """
+    def __init__(self):
+        self.binFileNames = []
+        self.dirName = []
+        self.dataToPlot = None
+        
+        
 def run():
     app = QApplication(sys.argv)
     GUI = Window()
